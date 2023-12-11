a64803478a66e98a6caa278cc0fc2e44a0e00821
Benjamin Zastrow <ben.g.zastrow@gmail.com>
2023-12-11 14:58:22 -0600

    Updates State object and related functions.
    Vector form is the underlying primary data structure, rather than matrix
    form. Also implemented padding and stripping functions for boundary and
    ghost nodes.

--- src/lid_driven_cavity/states.py
+++ src/lid_driven_cavity/states.py
@@ -1,24 +1,32 @@
 """Contains State class."""
 from dataclasses import dataclass, field
-import math
 import numpy as np
 
 
 @dataclass
 class State:
     """
-    Single state vector for state values across entire grid. Assumes square
-    grid!
+    Represents a State object for a single state type (u, v, or phi).
 
     Parameters
     ----------
-    vector : (Nx*Ny,)
+    entries : np.ndarray
+        vector or matrix representation of state
     Nx : int
+        number of grid points in x direction
     Ny : int
+        number of grid points in y direction
     """
-    vector: np.ndarray
+    entries: np.ndarray
     Nx: int
     Ny: int
+    vector: np.ndarray = field(init=False)
+
+    def __post_init__(self):
+        if self.entries.ndim == 1:
+            self.vector = self.entries
+        elif self.entries.ndim == 2:
+            self.vector = matrix_to_vector(self.entries)
 
     def ij(self, i, j):
         """
@@ -38,72 +46,84 @@ class State:
             bottom left of grid. Counts from bottom to top and from left to
             right.
         """
-        return self.vector[j*self.Nx+i]
+        return self.vector[lij(i, j, self.Nx)]
 
-    @property
-    def matrix(self):
-        """
-        Returns 2D array version of state vector.
+    def set_matrix(self, matrix):
+        """Update state using matrix form."""
+        self.vector = matrix_to_vector(matrix)
 
-        Returns
-        -------
-        matrix : (n,n)
-            2D ndarray for contour plotting, etc, with origin at bottom left
-            of grid. Note that this may need to be manipulated for certain
-            plotting functions.
-        """
-        # # convert to 2D
-        # matrix = self.vector.reshape((self.Ny, self.Nx))
+    def get_matrix(self):
+        """Get state in matrix form."""
+        return vector_to_matrix(self.vector, Nx=self.Nx, Ny=self.Ny)
 
-        # # flip vertically (flip rows)
-        # matrix = np.flip(matrix, axis=0)
+    def __repr__(self):
+        """Returns string representation of State."""
+        return np.array_str(self.get_matrix())
 
-        # return matrix
-        return get_matrix(self.vector, self.Nx, self.Ny)
+    def pad_boundaries(self):
+        """Pads state with zero boundaries."""
+        padded_matrix = pad_boundaries(self.get_matrix())
+        self.set_matrix(padded_matrix)
+        self.Nx = self.Nx + 2
+        self.Ny = self.Ny + 2
 
-
-def compute_u_hat(state, direction):
-    """
-    Spatially interpolates (averages) a state across a 2D grid.
-
-    Parameters
-    ----------
-    state : State
-        Representing u or v on a 2D grid.
-    direction : str
-        'x' or 'y'
-
-    Returns
-    -------
-    state_hat : State
-        Interpolated state, direction of grid is reduced by 1 in "direction".
-    """
-    if direction == 'x':
-        interpolated_matrix = (state.matrix[:, :-1] + state.matrix[:, 1:]) / 2.0
-        return interpolated_matrix
-        # return State(
-        #     get_vector(interpolated_matrix),
-        #     Nx=state.Nx-1,
-        #     Ny=state.Ny,
-        #     )
-    if direction == 'y':
-        interpolated_matrix = (state.matrix[:-1, :] + state.matrix[1:, :]) / 2.0
-        return interpolated_matrix
-        # return State(
-        #     get_vector(interpolated_matrix),
-        #     Nx=state.Nx,
-        #     Ny=state.Ny-1,
-        #     )
-    raise ValueError(f'direction = {direction:s} must equal "x" or "y" ')
+    def strip_boundaries(self):
+        """Strips boundaries from state."""
+        stripped_matrix = strip_boundaries(self.get_matrix())
+        self.set_matrix(stripped_matrix)
+        self.Nx = self.Nx - 2
+        self.Ny = self.Ny - 2
 
 
-def get_vector(matrix):
-    """Converts a matrix in State format back to a state vector."""
+def matrix_to_vector(matrix):
+    """Converts a matrix in State format to a State vector."""
     return np.flip(matrix, axis=0).reshape(-1,)
 
 
-def get_matrix(vector, Nx, Ny):
-    """Converts a vector in State format back to state matrix."""
-    matrix = vector.reshape((Ny, Nx))  # convert to 2D
-    matrix = np.flip(matrix, axis=0)  # flip vertically (flip rows)
-    return matrix
+def vector_to_matrix(vector, Nx, Ny):
+    """Converts a vector in State format to State matrix."""
+    return np.flip(vector.reshape((Ny, Nx)), axis=0)  # 2D then flip vertically
+
+
+def lij(i, j, Nx):
+    """Row major ordering indexing function."""
+    return j * Nx + i
+
+
+def pad_boundaries(matrix):
+    """
+    Adds a border of zeros around an existing matrix.
+
+    Increases x and y dimensions of array by 2 each.
+
+    Parameters
+    ----------
+    matrix : np.ndarray (Nx, Ny)
+        initial matrix to be padded with zeros.
+    Returns
+    -------
+    padded_matrix : np.ndarray (Nx+2, Ny+2)
+        original matrix with a border of zero entries added.
+    """
+    matrix_padded = np.zeros((matrix.shape[0]+2, matrix.shape[1]+2))
+    matrix_padded[1:-1, 1:-1] = matrix
+    return matrix_padded
+
+
+def strip_boundaries(matrix):
+    """
+    Removes the outermost row/column from each boundary of an existing matrix.
+
+    Reduces the x and y dimensions of an array by 2 each.
+
+    Parameters
+    ----------
+    matrix : np.ndarray (Nx, Ny)
+        initial matrix to be stripped of its boundary rows and columns.
+
+    Returns
+    -------
+    stripped_matrix : np.ndarray (Nx-2, Ny-2)
+        original matrix with its boundary rows and columns removed
+    """
+    return matrix[1:-1, 1:-1]
--- tests/test_states.py
+++ tests/test_states.py
@@ -2,119 +2,111 @@
 import pytest
 import numpy as np
 
-from lid_driven_cavity.states import State, interpolate, get_vector
+from lid_driven_cavity import states
+
+NX, NY = 4, 5  # number of grid points in x, y directions
 
 
-@pytest.fixture(name='i', params=range(5))
+@pytest.fixture(name='Nx')
+def create_mock_Nx():
+    """Mock Nx, i.e. number of grid points in x direction."""
+    return NX
+
+
+@pytest.fixture(name='Ny')
+def create_mock_Ny():
+    """Mock Ny, i.e. number of grid points in y direction."""
+    return NY
+
+
+@pytest.fixture(name='vector')
+def create_mock_vector():
+    """Mock state vector."""
+    vector = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+    return vector
+
+
+@pytest.fixture(name='matrix')
+def create_mock_matrix():
+    """Mock state matrix."""
+    matrix = np.array([
+        [16, 17, 18, 19],
+        [12, 13, 14, 15],
+        [ 8,  9, 10, 11],
+        [ 4,  5,  6,  7],
+        [ 0,  1,  2,  3],
+    ])
+    return matrix
+
+
+@pytest.fixture(name='matrix_padded')
+def create_mock_padded_matrix():
+    """Mock state matrix."""
+    matrix = np.array([
+        [0,  0,  0,  0,  0, 0],
+        [0, 16, 17, 18, 19, 0],
+        [0, 12, 13, 14, 15, 0],
+        [0,  8,  9, 10, 11, 0],
+        [0,  4,  5,  6,  7, 0],
+        [0,  0,  1,  2,  3, 0],
+        [0,  0,   0,  0,  0, 0],
+    ])
+    return matrix
+
+
+@pytest.fixture(name='i', params=range(NX))
 def create_mock_i(request):
     """Mock int value(s)."""
     return request.param
 
 
-@pytest.fixture(name='j', params=range(4))
+@pytest.fixture(name='j', params=range(NY))
 def create_mock_j(request):
     """Mock int value(s)."""
     return request.param
 
 
-def test_get_matrix():
+def test_matrix(Nx, Ny, vector, matrix):
     """Tests that the correct matrix is created from the vector."""
-    Nx, Ny = 5, 4
-    state = State(np.arange(0, Nx*Ny), Nx=Nx, Ny=Ny)
-    mock_matrix = np.array([
-        [15, 16, 17, 18, 19],
-        [10, 11, 12, 13, 14],
-        [5,   6,  7,  8,  9],
-        [0,   1,  2,  3,  4],
-    ])
-    assert mock_matrix == pytest.approx(state.matrix)
+    state = states.State(vector, Nx=Nx, Ny=Ny)
+    assert matrix == pytest.approx(state.get_matrix())
 
 
-def test_matrix_equals_ij(i, j):
+def test_vector(Nx, Ny, vector, matrix):
+    """Tests that the correct matrix is created from the vector."""
+    state = states.State(matrix, Nx=Nx, Ny=Ny)
+    assert vector == pytest.approx(state.vector)
+
+
+def test_ij(i, j, matrix, Nx, Ny):
     """Tests that ij indexing function matches matrix output."""
-    Nx, Ny = 5, 4
-    state = State(np.arange(0, Nx*Ny), Nx=Nx, Ny=Ny)
-    matrix = state.matrix
-    assert state.ij(i, j) == pytest.approx(matrix[state.Ny-j-1, i])
+    state = states.State(matrix, Nx=Nx, Ny=Ny)
+    assert state.ij(i, j) == pytest.approx(matrix[Ny-j-1, i])
 
 
-def test_get_vector_x():
-    """
-    Converts a matrix in State format back to a state vector.
-
-    Create a state vector.
-    Create a State object from the state vector.
-    Get the matrix version of the State.
-    Convert that matrix back to a vector again.
-    Compare to ensure no changes.
-    """
-    vector = np.arange(5*4)
-    state = State(vector, 5, 4)
-    matrix = state.matrix
-    new_vector = get_vector(matrix)
-    assert vector == pytest.approx(new_vector)
+def test_vector_changes(Nx, Ny, vector, matrix):
+    """Tests that updates to State.vector propogate correctly."""
+    state = states.State(vector, Nx=Nx, Ny=Ny)
+    state.vector[:Nx] = 100
+    matrix[-1, :] = 100
+    assert matrix == pytest.approx(state.get_matrix())
 
 
-def test_get_vector_y():
-    """
-    Converts a matrix in State format back to a state vector.
-
-    Create a state vector.
-    Create a State object from the state vector.
-    Get the matrix version of the State.
-    Convert that matrix back to a vector again.
-    Compare to ensure no changes.
-    """
-    vector = np.arange(4*5)
-    state = State(vector, 4, 5)
-    matrix = state.matrix
-    new_vector = get_vector(matrix)
-    assert vector == pytest.approx(new_vector)
+def test_matrix_changes(Nx, Ny, vector, matrix):
+    """Tests that updates to State.matrix propogate correctly."""
+    state = states.State(matrix, Nx=Nx, Ny=Ny)
+    matrix = state.get_matrix()
+    matrix[-1, :] = 100
+    state.set_matrix(matrix)
+    vector[:Nx] = 100
+    assert vector == pytest.approx(state.vector)
 
 
-def test_interpolate_x():
-    """Tests that the interpolation works in the x-direction."""
-    Nx, Ny = 5, 4
-    state = State(np.arange(Nx*Ny), Nx=Nx, Ny=Ny)
-    mock = np.array([
-        [15.5, 16.5, 17.5, 18.5],
-        [10.5, 11.5, 12.5, 13.5],
-        [5.5,  6.5,  7.5,  8.5],
-        [0.5,  1.5,  2.5,  3.5],
-        ])
-    actual = interpolate(state, direction='x')
-    assert mock == pytest.approx(actual.matrix)
+def test_pad_boundaries(matrix, matrix_padded):
+    """Tests pad_boundaries() method."""
+    assert matrix_padded == pytest.approx(states.pad_boundaries(matrix))
 
 
-def test_interpolate_y():
-    """Tests that the interpolation works in the y-direction."""
-    Nx, Ny = 4, 5
-    state = State(np.arange(Nx*Ny), Nx=Nx, Ny=Ny)
-    mock = np.array([
-        [14., 15., 16., 17.],
-        [10., 11., 12., 13.],
-        [6.,  7.,  8.,  9.],
-        [2.,  3.,  4.,  5.],
-        ])
-    actual = interpolate(state, direction='y')
-    assert mock == pytest.approx(actual.matrix)
-
-# # Testing get_vector <-> get_matrix
-# Nx, Ny = 5, 4
-# matrix = np.flip(np.arange(1, Nx*Ny+1).reshape((Ny, Nx)), axis=0)
-# print(matrix)
-# vector = states.get_vector(matrix)
-# print(vector)
-# matrix2 = states.get_matrix(vector, Nx, Ny)
-# print(matrix2)
-
-# # convert to vector
-# b = states.get_vector(b)
-# print('b to vector')
-# print(b)
-# bmatrix = states.get_matrix(b, 4, 5)
-# print('b vector to matrix')
-# print(bmatrix)
-# b2 = states.get_vector(bmatrix)
-# print('b back to vector')
-# print(b2)
+def test_strip_boundaries(matrix, matrix_padded):
+    """Tests strip_boundaries() method."""
+    assert matrix == pytest.approx(states.strip_boundaries(matrix_padded))
